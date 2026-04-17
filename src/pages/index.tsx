9b8b64b6406581a8fd7fc010d307d0fcb81a7ea7
Frank Zickert <mail@pyqml.com>
2026-04-17 21:22:23 +0200

    feat: docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -105,7 +105,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Fernsehturm Tutorial - 5min ⏱️
+            Read the Docs →
           </Link>
         </div>
       </div>
